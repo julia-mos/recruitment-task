ff49679c91a5e67ef66450861d5f44a3c0a861df
Jula <juliamos45@gmail.com>
2020-06-05 20:24:05 +0200

    Added login and register checking

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Login from "./Login"
+import Palindromes from "./Palindromes"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      users:[],
+      logged:false,
+      register:false
+    }
+    //Użytkownicy powinni logować się poprzez połączenie z bazą danych. Tam trzymane hasła są bezpieczne, 
+    //ponieważ nie ma ich po stronie frontednu oraz są szyfrowane. Trzymanie danych do logowania po
+    //stronie frontendu nie jest bezpiecznym, ani trwałym rozwiązaniem - dane znikają po ponownym uruchomieniu projektu
+    //a także trzymanie ich lokalnie (w state komponentu lub store reduxa) umożliwia do nich dostęp poprzez narzędzia developerskie reacta.
+  }
+  handleAction = (login,username,password) =>{
+    let users=this.state.users
+    if(login){
+      if(this.state.users.find(x=>x.username===username).password===password)
+        return true;
+      else
+        return false;
+    }
+    else{
+      if(this.state.users.find(x=>x.username===username)===undefined){
+      users.push({"username":username, "password":password})
+      this.setState({users: users})
+      return true;
+      }
+      else return false;
+    }
+  } 
+  render() {
+    return (
+      <div>
+        {
+        !this.state.logged?
+          <Login handleAction={this.handleAction}/>
+        :
+        <Palindromes />
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Login.js
+++ src/Login.js
@@ -1,40 +1,62 @@
 import React, { Component } from 'react';
-import {Base64} from "js-base64"
+import { Base64 } from "js-base64"
 
 class Login extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      username:"",
-      password:"",
-      wantToLogin:true,
-      message:"Don't have an account? Register now",
-      title: "Log in"
+    this.state = {
+      username: "",
+      password: "",
+      wantToLogin: true,
+      message: "Don't have an account? Register now",
+      title: "Login",
+      warning:""
     }
   }
-  handlePasswordChange = (e)=>{
-    this.setState({password: Base64.encode(e.target.value)})
+  handlePasswordChange = (e) => {
+    this.setState({ password: Base64.encode(e.target.value), warning:"" })
   }
-  handleUsernameChange = () =>{
-
+  handleUsernameChange = (e) => {
+    this.setState({ username: e.target.value,warning:"" })
   }
-  handleSubmit = (e) =>{
+  handleSubmit = (e) => {
     e.preventDefault();
+    if(this.state.username!=="" && this.state.password !=="")
+    {
+      if(this.props.handleAction(this.state.wantToLogin,this.state.username,this.state.password))
+        console.log("git")
+    }
+    else
+    this.setState({warning: "username and password cannot be empty"})
   }
-  handleChangingAction = () =>{
+  handleChangingAction = () => {
     this.state.wantToLogin ?
-    this.setState({wantToLogin : false, message: "Have an account? Log in now", title:"Register"})
-    :
-    this.setState({wantToLogin : true, message: "Don't have an account? Register now",title:"Log in"})
+      this.setState({
+        username: "",
+        password: "",
+        wantToLogin: false,
+        message: "Have an account? Log in now",
+        title: "Register",
+        warning:""
+      })
+      :
+      this.setState({
+        username: "",
+        password: "", wantToLogin: true,
+        message: "Don't have an account? Register now",
+        title: "Login",
+        warning:""
+      })
   }
   render() {
     return (
       <div>
-      {this.state.title} <br/>
-      <input type="text" value={this.state.username} placeholder="username" onChange={this.handleUsernameChange}/>
-      <input type="password" value={Base64.decode(this.state.password)} placeholder="password" onChange={this.handlePasswordChange}/>
-      <button type="submit" onClick={this.handleSubmit}>Login</button>
-      <button onClick={this.handleChangingAction}>{this.state.message}</button>
+        {this.state.title} <br />
+        <input type="text" value={this.state.username} placeholder="username" onChange={this.handleUsernameChange} />
+        <input type="password" value={Base64.decode(this.state.password)} placeholder="password" onChange={this.handlePasswordChange} />
+        <button type="submit" onClick={this.handleSubmit}>{this.state.title}</button>
+        <button onClick={this.handleChangingAction}>{this.state.message}</button>
+        <p color="red">{this.state.warning}</p>
       </div>
     );
   }
