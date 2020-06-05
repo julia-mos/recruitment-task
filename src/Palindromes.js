9331000995dd9f9cfffc5c790ecbc9e77faaf8c5
Jula <juliamos45@gmail.com>
2020-06-05 18:36:55 +0200

    Added files for the project structure

---
+++ src/Palindromes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Palindromes extends Component {
+  render() {
+    return (
+      <div> Palindromes </div>
+    );
+  }
+}
+
+export default componentName;
