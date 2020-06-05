c48718e1596aed4325e14037d9c0797f8903c09b
Jula <juliamos45@gmail.com>
2020-06-05 18:59:56 +0200

    Added components for Login form

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import {Base64} from "js-base64"
 
 class Login extends Component {
+  constructor(){
+    super();
+    this.state={
+      username:"",
+      password:""
+    }
+  }
+  handlePasswordChange = (e)=>{
+    this.setState({password: Base64.encode(e.target.value)})
+  }
+  handleUsernameChange = () =>{
+
+  }
+  handleSubmit = (e) =>{
+    e.preventDefault();
+  }
   render() {
     return (
-      <div> Login </div>
+      <div>
+      Log in <br/>
+      <input type="text" value={this.state.username} placeholder="username" onChange={this.handleUsernameChange}/>
+      <input type="password" value={Base64.decode(this.state.password)} placeholder="password" onChange={this.handlePasswordChange}/>
+      <button type="submit" onClick={this.handleSubmit}>Login</button>
+      <button onClick={this.props.changeAction}>Don't have an account? Register now</button>
+      </div>
     );
   }
 }
