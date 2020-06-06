92c620c9eef2f6e79011f52627658b19f7147125
Jula <juliamos45@gmail.com>
2020-06-06 15:11:17 +0200

    imported json file to created store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
-// import {createStore} from 'redux';
-// import reducers from './reducers';
-// import storeData from "./data.json"
+import {createStore} from 'redux';
+import reducers from './reducers';
+import storeData from "./data.json"
 
 
-// export const store = createStore(reducers,storeData);
+export const store = createStore(reducers,storeData);
