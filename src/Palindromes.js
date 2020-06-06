aec8df2e3cdafd5336fe1fd161ba77b2dbe36a1d
Jula <juliamos45@gmail.com>
2020-06-06 17:18:22 +0200

    Added styles for palindromes list

--- src/Palindromes.js
+++ src/Palindromes.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 import PalindromesList from "./PalindromesList"
+import "./style.css"
 
 
 class Palindromes extends Component {
   constructor(props){
     super(props);
-    console.log(props)
     this.state={inputValue:""}
   }
   handleInputChange = (e) =>{
     this.setState({inputValue:e.target.value})
   }
   handleSubmitClick = (e)=>{
-    e.preventDefault()
+    e.preventDefault()    
     this.props.addNewString(this.state.inputValue)
+    this.setState({inputValue: ""})
   }
   render() {
     return (
-      <div>
-        <label>Type a string to check</label>
-        <input type="text" value={this.state.value} onChange={this.handleInputChange}/>
-        <button type="submit" onClick={this.handleSubmitClick}>Check it out!</button>
+      <div className="wrapper">
+      <div className="formContainer">
+        <h1>Type a string</h1>
+        <input className="formInput" placeholder="check" type="text" value={this.state.inputValue} onChange={this.handleInputChange}/>
+        <button className="formBtn" type="submit" onClick={this.handleSubmitClick}>Check it out!</button>
         <PalindromesList list={this.props.palindromes} />
+      </div>
       </div>
     );
   }
--- src/PalindromesList.js
+++ src/PalindromesList.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import "./style.css"
+import yes from "./imgs/ok.png"
+import no from "./imgs/nie.png"
 
 class PalindromesList extends Component {
   render() {
     let list=this.props.list.map((obj,index) =>
-    <li key={index}>{obj.text} : {obj.isPalindrome.toString()}</li>
+    <li className="listItem" key={index}><span>{obj.text}</span> : {
+      obj.isPalindrome?
+      <img src={yes} alt="It is palindrome"/>
+      :
+      <img src={no} alt="It isn't palindrome"/>
+      }</li>
     )
     return (
-    <ul>{list}</ul>
+    <ul className="list">{list}</ul>
     );
   }
 }
