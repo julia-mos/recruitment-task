78055f9f7d6d7c0318c63af25697ea80df6b9efe
Jula <juliamos45@gmail.com>
2020-06-06 15:26:15 +0200

    getting data from redux store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Login from "./Login"
 import Palindromes from "./Palindromes"
+import { connect } from "react-redux";
+import { palindromesFetched } from "./actions";
 
 class App extends Component {
   constructor(){
@@ -14,6 +16,9 @@ class App extends Component {
     //ponieważ nie ma ich po stronie frontednu oraz są szyfrowane. Trzymanie danych do logowania po
     //stronie frontendu nie jest bezpiecznym, ani trwałym rozwiązaniem - dane znikają po ponownym uruchomieniu projektu
     //a także trzymanie ich lokalnie (w state komponentu lub store reduxa) umożliwia do nich dostęp poprzez narzędzia developerskie reacta.
+  }
+  componentDidMount = () =>{
+    this.props.palindromesFetched();
   }
   handleAction = (login,username,password) =>{
     let users=this.state.users
@@ -43,11 +48,19 @@ class App extends Component {
         !this.state.logged?
           <Login handleLogging={this.handleLogging} handleAction={this.handleAction}/>
         :
-        <Palindromes />
+        <Palindromes palindromes={this.props.palindromes}/>
         }
       </div>
     );
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    palindromesObject: state.palindromes
+  }
+};
+const mapDispatchToProps = { palindromesFetched };
+
+export const AppContainer = connect(mapStateToProps, mapDispatchToProps)(App);
 export default App;
