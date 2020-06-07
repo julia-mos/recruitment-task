e134257bdba1eb91f755ebc5d950d14741c6097c
Jula <juliamos45@gmail.com>
2020-06-07 10:14:55 +0200

    Created testing cases

--- __tests__/checkString.test.js
+++ __tests__/checkString.test.js
@@ -1,1 +1,13 @@
 const Check = require('../src/checkString')
+
+test('kajak', () =>{
+    expect(Check('kajak').toBe(true))
+})
+
+test('mama', () =>{
+    expect(Check('mama').toBe(false))
+})
+
+test('Zakopane na pokaz', () =>{
+    expect(Check('Zakopane na pokaz').toBe(true))
+})
